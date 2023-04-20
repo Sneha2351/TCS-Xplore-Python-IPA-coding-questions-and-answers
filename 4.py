a2632f290c93d3af7f65eb6814a756dd138a516d
Sneha Sharma <68264594+Sneha2351@users.noreply.github.com>
2023-04-20 16:00:16 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+n = int (input())
+sum = 0
+for i in str(n):
+    sum += int(i)
+print(sum)
